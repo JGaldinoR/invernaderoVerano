b41a4f7fc5268765d4f134849009c71afb6328cf
Usuario <Usuario>
2017-06-15 11:11:26 -0500

    agregar usuarios

--- coneccion.py
+++ coneccion.py
@@ -11,5 +11,11 @@ def mostrar_usuario():
 	for fila in r:
 		print(fila)
 
+def insertar_usuario(datos):
+	c.execute("INSERT INTO usuario (nombre, apellido1,apellido2, correo, password, tipo) VALUES(?,?,?,?,?,?)", (datos[0], datos[2],datos[3],datos[4],datos[5]))
+	db.commit()
+
+
+insertar_usuario(["m","d","b","m@gmail.com","l",1])
 
 mostrar_usuario()
