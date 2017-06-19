9fe5cb7c0d1cc89f1c055684a118147199ccc5b4
Usuario <Usuario>
2017-06-19 11:32:27 -0500

    menu con insertar

--- menu.py
+++ menu.py
@@ -12,15 +12,25 @@ class Menu:
 			if op == "1":
 				self.mostrar()
 			elif op == "2":
-				pass
+				self.capturar()
 			elif op == "0":
 				break
 
 	def mostrar(self):
 		usuarios = self.inv.mostrar_usuario()
 		for u in usuarios:
-			print("{0:2} {1:10} {2:8} {3:8} {4:15} {5:6} {6:1}".format(u[0].u[1],u[2],u[3],u[4],u[5],u[6]))
+			print("{0:2} {1:10} {2:8} {3:8} {4:15} {5:6} {6:1}".format(u[0],u[1],u[2],u[3],u[4],u[5],u[6]))
 
+
+	def capturar(self):
+		nombre = input("Nombre: ")
+		apellido1 = input("Apellido 1: ")
+		apellido2 = input("Apellido 2: ")
+		correo = input("Correo: ")
+		password = input("Password: ")
+		tipo = input("Tipo: ")
+
+		self.inv.insertar_usuario([nombre, apellido1,apellido2, correo, password, tipo])
 
 
 
